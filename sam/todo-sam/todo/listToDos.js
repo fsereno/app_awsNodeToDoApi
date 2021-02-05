700d18c73da7347b118232c7053c15decc71917e
Fabio Sereno <fabio_sereno@hotmail.com>
2021-02-05 22:17:33 +0000

    Started converting to the sam app, which will become the project eventually

---
+++ sam/todo-sam/todo/listToDos.js
@@ -0,0 +1,48 @@
+
+const AWS = require("aws-sdk");
+const dynamo = new AWS.DynamoDB.DocumentClient();
+const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
+
+exports.handler = async (event) => {
+
+    const accessToken = event.headers.access;
+
+    let username = "in28minutes";
+
+    if (accessToken) {
+
+        const identity = {
+            AccessToken: accessToken
+        };
+
+        let user = await cognitoidentityserviceprovider.getUser(identity).promise();
+
+        if (user) {
+            username = user.Username;
+        }
+
+    }
+
+    let params = {
+        TableName : 'todo',
+        FilterExpression : 'username = :username',
+        ExpressionAttributeValues : {':username' : username}
+    };
+
+    const result = await dynamo.scan(params).promise();
+
+    const statusCode = 200;
+
+    const body = JSON.stringify(result.Items);
+
+    const headers = {
+        "Access-Control-Allow-Origin": "*"
+    }
+
+    const response = {
+        statusCode,
+        body,
+        headers
+    };
+    return response;
+};
