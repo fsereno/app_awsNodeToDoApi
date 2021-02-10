8176e914f7cacdbf775343b0f2004eb25433661b
Fabio Sereno <fabio_sereno@hotmail.com>
2021-02-10 22:18:10 +0000

    Added sam getToDo

---
+++ sam/todo-sam/todo/getToDo.js
@@ -0,0 +1,29 @@
+const AWS = require("aws-sdk");
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event) => {
+
+    const id = event.pathParameters.id;
+
+    let params = {
+        TableName : process.env.TODO_TABLE,
+        Key: { id }
+    };
+
+    const result = await dynamo.get(params).promise();
+
+    const statusCode = 200;
+
+    const body = JSON.stringify(result.Item);
+
+    const headers = {
+        "Access-Control-Allow-Origin": "*"
+    }
+
+    const response = {
+        statusCode,
+        body,
+        headers
+    };
+    return response;
+};
