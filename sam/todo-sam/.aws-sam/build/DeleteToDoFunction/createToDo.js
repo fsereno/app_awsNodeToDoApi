482d123bbc7b14062e0715b6b88708a27b881a6f
Fabio Sereno <fabio_sereno@hotmail.com>
2021-02-10 22:40:26 +0000

    Added sam create and delete todos

---
+++ sam/todo-sam/.aws-sam/build/DeleteToDoFunction/createToDo.js
@@ -0,0 +1,34 @@
+const AWS = require("aws-sdk");
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event) => {
+
+    const item = JSON.parse(event.body);
+
+    if (!item.id || item.id === "-1") {
+
+        const id =  Math.random() * Math.pow(10,16);
+
+        item.id = id.toString();
+    }
+
+    let params = {
+        TableName : process.env.TODO_TABLE,
+        Item: item
+    };
+
+    await dynamo.put(params).promise();
+
+    const statusCode = 200;
+
+    const headers = {
+        "Access-Control-Allow-Origin": "*"
+    }
+
+    const response = {
+        statusCode,
+        body:"",
+        headers
+    };
+    return response;
+};
