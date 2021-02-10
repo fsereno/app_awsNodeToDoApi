d31bdeede84df6cd9e7bded9d03e7b1d5e133f11
Fabio Sereno <fabio_sereno@hotmail.com>
2021-02-10 22:31:08 +0000

    Added sam updateToDo

---
+++ sam/todo-sam/todo/updateToDo.js
@@ -0,0 +1,27 @@
+const AWS = require("aws-sdk");
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event) => {
+
+    const body = JSON.parse(event.body);
+
+    let params = {
+        TableName : process.env.TODO_TABLE,
+        Item: body
+    };
+
+    await dynamo.put(params).promise();
+
+    const statusCode = 200;
+
+    const headers = {
+        "Access-Control-Allow-Origin": "*"
+    }
+
+    const response = {
+        statusCode,
+        body:"",
+        headers
+    };
+    return response;
+};
