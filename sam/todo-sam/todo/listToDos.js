e6b73a18d4b2c1ceeaa17698ea269319ad178f8f
Fabio Sereno <fabio_sereno@hotmail.com>
2021-03-16 15:10:43 +0000

    Fixed both Sam and Serverless pocs to now get username from jwt claim instead of calling a method from cognito

--- sam/todo-sam/todo/listToDos.js
+++ sam/todo-sam/todo/listToDos.js
@@ -1,27 +1,10 @@
 
 const AWS = require("aws-sdk");
 const dynamo = new AWS.DynamoDB.DocumentClient();
-const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
 
 exports.handler = async (event) => {
 
-    const accessToken = event.headers.access;
-
-    let username = "in28minutes";
-
-    if (accessToken) {
-
-        const identity = {
-            AccessToken: accessToken
-        };
-
-        let user = await cognitoidentityserviceprovider.getUser(identity).promise();
-
-        if (user) {
-            username = user.Username;
-        }
-
-    }
+    const username = event.requestContext.authorizer.jwt.claims['cognito:username'];
 
     let params = {
         TableName : process.env.TODO_TABLE,
